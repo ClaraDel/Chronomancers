abe0df02289493183fe17c58ede5120e85185947
KorobeiJT <jtdemulatier@gmail.com>
2022-02-10 11:57:42 -0500

    Merge cancelled

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CursorManager : MonoBehaviour
 {
+
+    private int positionX; 
+    private int positionY;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,25 @@ public class CursorManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow)) 
+        { 
+            positionY+=1; 
+            transform.Translate(new Vector3(0,1,0)); 
+        } 
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) 
+        { 
+            positionY-=1; 
+            transform.Translate(new Vector3(0,-1,0)); 
+        } 
+        else if (Input.GetKeyDown(KeyCode.LeftArrow)) 
+        { 
+            positionX-=1; 
+            transform.Translate(new Vector3(-1,0,0)); 
+        } 
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) 
+        { 
+            positionX+=1; 
+            transform.Translate(new Vector3(1,0,0)); 
+        } 
     }
 }
