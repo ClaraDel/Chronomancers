edb2630e0cbfbfc99658b8e0eecbe484084d583f
Mike <mike.heck@hotmail.com>
2022-02-16 17:35:26 -0500

    Nouvelles images + options ajoutées

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audiomixer;
+
     public void StartGame()
     {
         Debug.Log("START");
+        SceneManager.LoadScene("JT");
     } 
     
     public void StartTutorial()
     {
         Debug.Log("TUTORIAL");
+        SceneManager.LoadScene("JT");
     } 
+
+    public void SetFullscreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+
+    public void SetMainVolume(float volume)
+    {
+        audiomixer.SetFloat("MainVolume", volume);
+    }
+    public void SetSFXVolume(float volume)
+    {
+        audiomixer.SetFloat("SFXVolume", volume);
+    }
+    public void SetMusicVolume(float volume)
+    {
+        audiomixer.SetFloat("MusicVolume", volume);
+    }
+
+
+
     public void QuitGame()
     {
         Application.Quit();
